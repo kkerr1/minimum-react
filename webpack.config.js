ee38597488cb4b89aad87b1932cd4c851056f9ce
Kevin Kerr <kevin.kerr@formidable.com>
2017-05-26 10:49:11 -0700

    tweaks for dashboard compat

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const DashboardPlugin = require('webpack-dashboard/plugin');
 module.exports = {
   // You may want 'eval' instead if you prefer to see the compiled output in DevTools.
   // See the discussion in https://github.com/facebookincubator/create-react-app/issues/343.
-  devtool: 'eval',
+  devtool: 'source-maps',
   // These are the "entry points" to our application.
   // This means they will be the "root" imports that are included in JS bundle.
   // The first two entry points enable "hot" CSS and auto-refreshes for JS.
